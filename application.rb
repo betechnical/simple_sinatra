d803c8df1d705789c104010c1f83dc273a934e0c
Sarah Seegal <sarahseegal@Sarah-Seegals-MacBook-Air.local>
2011-11-12 15:55:58 -0800

    fixed pluralization

--- application.rb
+++ application.rb
@@ -5,9 +5,15 @@ get "/" do
 end
 
 get "/delicious/:delicious_thing" do
-  if params[:delicious_thing] == "cats"
+  #if params[:delicious_thing] == "cats" and we edited this out with the hash
+if params[:delicious_thing][-1] == "s"
     "#{params[:delicious_thing]} are delicious"
   else
     "#{params[:delicious_thing]} is delicious"
   end   
+
+	 
+
+
+
 end
