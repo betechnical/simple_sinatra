9abe9edfdb469aaa8d44f816550bb24b6c6e0e1a
Katie <katiejohnsonsf@gmail.com>
2011-11-12 15:10:50 -0800

    took all the fun out of it

--- application.rb
+++ application.rb
@@ -4,10 +4,10 @@ get "/" do
     "this is the best"
 end
 
-get "/fun/:fun_thing" do
-  if params[:fun_thing] == "cats"
-    "#{params[:fun_thing]} are fun"
+get "/delicious/:delicious_thing" do
+  if params[:delicious_thing] == "cats"
+    "#{params[:delicious_thing]} are delicious"
   else
-    "#{params[:fun_thing]} is fun"
+    "#{params[:delicious_thing]} is delicious"
   end   
 end
