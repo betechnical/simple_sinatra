f47059728a73c5af59ddf0bd94f2e2bf5dead188
Melinda Byerley <mjb82@cornell.edu>
2011-11-17 20:49:51 -0800

    cleaned up code structure

--- application.rb
+++ application.rb
@@ -9,15 +9,11 @@ end
 
 get "/delicious/:delicious_thing" do
 
-if params[:delicious_thing] == params[:delicious_thing].pluralize
-"#{params[:delicious_thing]} are delicious"
-
-else    
- "#{params[:delicious_thing]} is delicious"
-
-end
- 
-
+	if params[:delicious_thing] == params[:delicious_thing].pluralize
+		"#{params[:delicious_thing]} are delicious"
+	else    
+ 		"#{params[:delicious_thing]} is delicious"
+	end 
 end 
 
 	 
